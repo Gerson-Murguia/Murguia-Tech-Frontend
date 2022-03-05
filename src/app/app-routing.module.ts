4acfd4da99cf7a11e4a5bf4db13119a46e93430c
GersonMurguia <aldmurguia1@gmail.com>
2022-03-05 14:09:32 -0500

    Ultimo cambio al router module revertido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-{path:'/login', component: LoginComponent},
-{path:'/registro', component:RegisterComponent},
+{path:'login', component: LoginComponent},
+{path:'registro', component:RegisterComponent},
 {path:'', redirectTo:'/login', pathMatch:'full'},
-{path:'/user/administracion', component:UserComponent,canActivate: [AuthenticationGuard]}
+{path:'user/administracion', component:UserComponent,canActivate: [AuthenticationGuard]}
 ];
 
 @NgModule({
